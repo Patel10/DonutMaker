75114a9e055a7d82432470e6bac4b08c42c11d21
Patel10 <parth1029@yahoo.com>
2021-07-09 22:58:37 -0400

    working auto clicker not multiplier

---
+++ DonutMaker.js
@@ -0,0 +1,13 @@
+class DonutMaker {
+    constructor(numDonuts, numAutoClickers){
+        this.numDonuts = numDonuts;
+        this.numAutoClicker= numAutoClickers;
+    }
+    
+addDonut(){
+    this.numDonuts++;
+}
+}
+
+
+export default DonutMaker;
